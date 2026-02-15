5c1a1dd52b46c47e92e42dbef53b8660835be0b6
nabilbouga <nabil.bouga.2016@gmail.com>
2026-02-15 14:34:27 +0000

    css problem

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  
   typescript: {
     ignoreBuildErrors: true,
   },
    output: "export",
+    basePath: "/Towkeyshouse",
+    assetPrefix: "/Towkeyshouse",
   images: {
     unoptimized: true,
   },
