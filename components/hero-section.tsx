648453ce313a2d5a268c4104dc14caba6be22240
nabilbouga <nabil.bouga.2016@gmail.com>
2026-02-17 13:35:25 +0000

    fix Add videol

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -10,7 +10,7 @@ export function HeroSection() {
         loop
         className="absolute inset-0 w-full h-full object-cover"
       >
-        <source src="https://videos.pexels.com/video-files/1409899/1409899-hd_1920_1080_30fps.mp4" type="video/mp4" />
+        <source src="/Videobackground.mp4" type="video/mp4" />
       </video>
 
       {/* Overlay */}
